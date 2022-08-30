dd34a09944bbe564c00268cd2fd7c1581f149418
Anisha Iyer <anishaiyer27@gmail.com>
2022-08-30 01:16:56 -0700

    committing functioning react app with simple display

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-//import logo from './NTB Blue Wordmark White.svg'
+//import logo from './logo.svg';
+import logo from './word_logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <link href="https://cdn.jsdelivr.net/npm/@chartshq/muze@2.0.0/dist/muze.css" rel="stylesheet"></link>
-        <script src="https://cdn.jsdelivr.net/npm/@chartshq/muze@2.0.0/dist/muze.js" type="text/javascript"></script>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Neurotech Fa'22 Infosession Demo!
+          Neurotech@Berkeley Fall '22 Infosession Demo
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://neurotech.berkeley.edu/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Neurotech Website
         </a>
       </header>
     </div>
